b2854923fa39229eabd9f9aba372b3a0f6df488f
kunky <kunky>
2005-04-11 09:53:19 +0000

    Filedialog remembers last opened file

--- pygtkplanter/planter.py
+++ pygtkplanter/planter.py
@@ -22,6 +22,7 @@ class planter:
 	def __init__(self):
 		gladefile = "planter.glade"
 		windowname = "window1"
+		self.lastfilename = ""
 		self.planting = 0
 		self.window = gtk.glade.XML(gladefile, windowname)
 		self.window.get_widget("window1").connect("delete_event", self.destroy)
@@ -35,13 +36,19 @@ class planter:
 		time = time.ctime()
 		self.iter = self.logbuffer.get_iter_at_offset(0)
 		self.logbuffer.insert_with_tags_by_name(self.iter, time+": Program Loaded", "blue_foreground")
-		self.logbuffer.insert_with_tags_by_name(self.iter, "\rAnatomic P2P Planter CVS Edition\rLicenced under the MIT Licence", "blue_foreground")
+		self.logbuffer.insert_with_tags_by_name(self.iter, "\rAnatomic P2P Planter CVS Edition\rLicenced under the MIT / X Consortium Licence", "blue_foreground")
 	def browsefile(self, widget, data=None):
-		self.lastdialog = "filechooserdialog1"
 		gladefile = "planter.glade"
 		dialogname = "filechooserdialog1"
 		self.dialog = gtk.glade.XML(gladefile, dialogname)
+		if self.lastfilename != "":
+			self.dialog.get_widget("filechooserdialog1").set_filename(self.lastdir)
 		self.dialog.get_widget("button4").connect_object("clicked", self.dialogdestroy, dialogname)
+		self.dialog.get_widget("button5").connect("clicked", self.takefile)
+	def takefile(self, widget, data=None):
+		filename = self.dialog.get_widget("filechooserdialog1").get_filename()
+		self.dialog.get_widget("filechooserdialog1").destroy()
+		self.lastfilename = filename 
 	def destroy(self, widget, data=None):
 		if self.planting == 1:
 			gladefile = "planter.glade"
@@ -57,7 +64,7 @@ class planter:
 			self.dialog.get_widget("cancelbutton1").connect_object("clicked", self.dialogdestroy, dialogname)
 		  	self.dialog.get_widget("dialog1").connect_object("delete_event", self.dialogdestroy, dialogname)
 			return True
-	def dialogdestroy(self, dialogname):
+	def dialogdestroy(self, dialogname, z=None):
 		self.dialog.get_widget(dialogname).destroy()
 	def byebye(self, widget, data=None):
 		gtk.main_quit()
