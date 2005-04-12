07d75db63a7447f2f4cacb2d948a817c0b00a03e
kunky <kunky>
2005-04-12 18:11:50 +0000

    Confirm Dialogs appear at correct times - ready to add code to plant torrent file

--- pygtkplanter/planter.py
+++ pygtkplanter/planter.py
@@ -27,6 +27,8 @@ class planter:
 		self.window = gtk.glade.XML(gladefile, windowname)
 		self.window.get_widget("window1").connect("delete_event", self.destroy)
 		self.window.get_widget("button2").connect("clicked", self.destroy)
+		self.window.get_widget("button3").connect("clicked", self.check)
+		#self.window.get_widget("button2").unset_flags(gtk.SENSITIVE)
 		self.window.get_widget("button1").connect("clicked", self.browsefile)
 		self.log = self.window.get_widget("textview1")
 		self.logbuffer = gtk.TextBuffer(None)
@@ -37,6 +39,23 @@ class planter:
 		self.iter = self.logbuffer.get_iter_at_offset(0)
 		self.logbuffer.insert_with_tags_by_name(self.iter, time+": Program Loaded", "blue_foreground")
 		self.logbuffer.insert_with_tags_by_name(self.iter, "\rAnatomic P2P Planter CVS Edition\rLicenced under the MIT / X Consortium Licence", "blue_foreground")
+	def check(self, widget, data=None):
+		if self.window.get_widget("entry1").get_text() == "":
+			gladefile = "planter.glade"
+			dialogname = "dialog4"
+			self.dialog = gtk.glade.XML(gladefile, dialogname)
+			self.dialog.get_widget("dialog4").connect_object("delete_event", self.dialogdestroy, dialogname)
+			self.dialog.get_widget("button8").connect_object("clicked", self.dialogdestroy, dialogname)
+		else:
+			gladefile = "planter.glade"
+			dialogname = "dialog3"
+			self.dialog = gtk.glade.XML(gladefile, dialogname)
+			self.dialog.get_widget("dialog3").connect_object("delete_event", self.dialogdestroy, dialogname)
+			self.dialog.get_widget("button6").connect_object("clicked", self.dialogdestroy, dialogname)
+			self.dialog.get_widget("button7").connect("clicked", self.startplant)
+	def startplant(self, widget, data=None):
+		self.dialog.get_widget("dialog3").destroy()
+		# need to code rest of plant - CVS Upload 20050412		
 	def browsefile(self, widget, data=None):
 		gladefile = "planter.glade"
 		dialogname = "filechooserdialog1"
@@ -75,5 +94,8 @@ class planter:
 		self.dialog.get_widget(dialogname).destroy()
 	def byebye(self, widget, data=None):
 		gtk.main_quit()
-app = planter()
-gtk.main()
+try:
+	app = planter()
+	gtk.main()
+except KeyboardInterrupt:
+	sys.exit(1)
