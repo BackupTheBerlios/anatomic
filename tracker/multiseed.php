0a48c5ee2c0236978f26c1df1af1ed5ae730122c
kunky <kunky>
2005-06-11 21:24:48 +0000

    bad peer share bug fixed

--- tracker/multiseed.php
+++ tracker/multiseed.php
@@ -1,6 +1,6 @@
 <?php
 /*
-    Anatomic P2P FBT Peer sharing script 0.2 BETA
+    Anatomic P2P FBT Peer sharing script 0.2 beta2
     Copyright (C) 2005  kunky
     You should have received a copy of the GNU General Public License
     along with this program; if not, write to the Free Software
@@ -8,74 +8,75 @@
 */
 // Current Project Status: Beta
 // Current Mental Status: Bored with lessons. (They know who they are(!)) Need mental challenge.
-// The data format is BEncode(array( hash => array( xxx.xxx.xxx.xxx:port:$t_time, xxx... )))
 include "BDecode.php";
-if(isset($_GET['info_hash']) && isset($_GET['data'])){
-$info_hash = $_GET['info_hash'];
-        if(strlen($info_hash) != 20)
+if(isset($_GET['info_hash']) && isset($_GET['data']))
 {
-        $info_hash = stripcslashes($_GET['info_hash']);
+$info_hash = $_GET['info_hash'];
+// Checks hash is ok
+if(strlen($info_hash) != 20)
+{
+$info_hash = stripcslashes($_GET['info_hash']);
 }
 if(strlen($info_hash) != 20)
 {
-                die("HASHFAIL");
+die("HASHFAIL");
 }
 $info_hash = bin2hex($info_hash);
-if(!file_exists($info_hash) && !file_exists("./multiseed/$info_hash")){
-        die("EXPIRED");
-        }
-$data = $_GET['data'];
-$data = @BDecode($data);
-if(!$data){
-        die("CORRUPT") ;
-        }
-foreach ($data as $ip){
-        $iparray = explode(":",$ip);
-        $ip2 = $iparray[0];
-        $port = $iparray[1];
-         $time = $iparray[2];
-                $peer_ip = explode('.', $ip2);
-$peer_ip = pack("C*", $peer_ip[0], $peer_ip[1], $peer_ip[2], $peer_ip[3]);
-$peer_port = pack("n*", (int)$port);
-$time = pack("C", $time);
+if(!file_exists($info_hash) && !file_exists("./multiseed/$info_hash"))
+{
+die("EXPIRED");
+}
+$data = urldecode($_GET['data']);
+$bdata = @BDecode($data);
+// $bdata is an array of ip addresses:ports:time
+if(!$bdata)
+{
+die("CORRUPT") ;
+}
 $handle = fopen($info_hash, "rb+");
 flock($handle, LOCK_EX);
 $peer_num = intval(filesize($info_hash) / 7);
 $data = fread($handle, $peer_num * 7);
 $peer = array();
-$updated = false;
+foreach ($bdata as $ip)
+{
+      $iparray = explode(":",$ip);
+      $ip2 = $iparray[0];
+      $port = $iparray[1];
+      $time = $iparray[2];
+      $peer_ip = explode('.', $ip2);
+      $peer_ip = pack("C*", $peer_ip[0], $peer_ip[1], $peer_ip[2], $peer_ip[3]);
+      $peer_port = pack("n*", (int)$port);
+      $time = pack("C", (int)$time);
+      $updated = 0;
 for($i=0;$i<$peer_num;$i++)
 {
         if($peer_ip . $peer_port == substr($data, $i * 7 + 1, 6))
         {
-                $updated = true;
-                if($_GET['event'] == 'stopped')
-                {
-                        $peer_num--;
-                }
-                else
-                {
-                        $peer[$i] = $time . $peer_ip . $peer_port;
-                }
-
+                $updated = 1;
+                $peer[$i] = $time . $peer_ip . $peer_port;
         }
         else
         {
-                $peer[$i] = substr($data, $i * 7, 7);
+                // WOOHOO I FINALLY FIXED THAT BUG
+                if(($addr = substr($data, $i * 7, 7)) != false)
+                {
+                    $peer[$i] = $addr;
+                }
         }
 }
-if($updated == false)
+if($updated == 0)
 {
         $peer[] = $time . $peer_ip . $peer_port;
         $peer_num++;
 }
 
+}
 rewind($handle);
 ftruncate($handle, 0);
 fwrite($handle, join('', $peer), $peer_num * 7);
 flock($handle, LOCK_UN);
 fclose($handle);
-                }
         die("ACCEPTED");
         }
 
